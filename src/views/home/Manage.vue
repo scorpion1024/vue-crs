3fc3f7a1e46daef1abf1d6cc16608f7ded82f852
caochenguang <scorpion198@163.com>
2021-12-30 17:10:44 +0800

    增加echart

--- src/views/home/Manage.vue
+++ src/views/home/Manage.vue
@@ -3,7 +3,38 @@
 </template>
 
 <script>
-export default {};
+import * as echarts from "echarts";
+export default {
+  data() {
+    return {
+      seriesData: [],
+      legendData : { left: "20px", orient: "horizontal", data: [] }
+    };
+  },
+  mounted() {
+    this.$api
+      .getWeaherData()
+      .then((res) => {
+        this.seriesData = res.echart.map(item => {
+            let data = {
+              name: item.name,
+              type: "line",
+              smooth: false,
+              data: [],
+            };
+            this.legendData.data.push(item.name);
+            item.data.forEach((value) => {
+              data.data.push([new Date(value[0]), value[1]]);
+            });
+            return data;
+          })
+        console.log(this.seriesData);
+      })
+      .catch((res) => {
+        console.log(res);
+      });
+  },
+};
 </script>
 
 <style>
